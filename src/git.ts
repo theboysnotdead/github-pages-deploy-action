458a3b310d0d92fe10d0ca2991d0ceac2d142085
James Ives <iam@jamesiv.es>
2019-10-16 20:56:26 -0400

    x

--- src/git.ts
+++ src/git.ts
@@ -23,7 +23,7 @@ export async function init() {
   }
 
   //await execute(`cd ${process.env.GITHUB_WORKSPACE}`);
-  await execute(`cd ${folder}`);
+  await execute(`cd ./${folder}`);
   await execute(`git init`);
   await execute(`git config user.name ${pusher.name}`);
   await execute(`git config user.email ${pusher.email}`);
