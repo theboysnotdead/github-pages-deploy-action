7e199dc6cb538351c3e914b099b16e128041d62f
James Ives <iam@jamesiv.es>
2019-10-16 21:43:47 -0400

    aabbccddeeddff

--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,14 @@
 import { exec } from "child_process";
+import { rejects } from "assert";
 
 export async function execute(cmd: string): Promise<String> {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     exec(cmd, (error, stdout) => {
+      if (error) {
+        reject(error)
+
+        console.log(error)
+      }
       resolve(stdout.trim());
     });
   });
