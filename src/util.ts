bd5a15b22e13eb5101d5cedb4d455a5294e2a914
James Ives <iam@jamesiv.es>
2019-10-18 20:41:46 -0400

    test?

--- src/util.ts
+++ src/util.ts
@@ -2,8 +2,8 @@ import { exec } from "@actions/exec";
 import { rejects } from "assert";
 
 export async function execute(cmd: string, cwd: string): Promise<String> {
-  return new Promise((resolve, reject) => {
-    exec(cmd, [], {
+  return new Promise(async (resolve, reject) => {
+    await exec(cmd, [], {
       cwd, 
       listeners: {
         stdout: (data: Buffer) => {
